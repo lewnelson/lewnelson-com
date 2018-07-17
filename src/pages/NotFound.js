4f96ff45a5d882e5294eabc404607a46ecc2de93
Lewis Nelson <lewis.nelson@holidayextras.com>
2018-07-17 16:14:15 +0100

    Updated not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import { Img } from '../components'
+import './notFound.scss'
 
 export default class NotFound extends Component {
   render () {
     return (
-      <h1>NotFound</h1>
+      <div id='not-found-page'>
+        <div className='page-container middle'>
+          <h1>Page Not Found</h1>
+          <div className='not-found-graphic'>
+            <div className='tumbleweed-container'>
+              <div className='background-tumbleweed-1'>
+                <Img
+                  src='/images/tumbleweed.png'
+                  lowQualitySrc='/images/tumbleweed-thumbnail.png'
+                  alt='tumbleweed'
+                />
+              </div>
+              <div className='background-tumbleweed-2'>
+                <Img
+                  src='/images/tumbleweed.png'
+                  lowQualitySrc='/images/tumbleweed-thumbnail.png'
+                  alt='tumbleweed'
+                />
+              </div>
+              <div className='foreground-tumbleweed'>
+                <Img
+                  src='/images/tumbleweed.png'
+                  lowQualitySrc='/images/tumbleweed-thumbnail.png'
+                  alt='tumbleweed'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
